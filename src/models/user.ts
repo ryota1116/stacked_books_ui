a39ab6bd440439509ceb54b64a83d70aecb12763
ryota1116 <ryota1116funa@gmail.com>
2022-07-01 11:40:03 +0900

    ユーザーモデルの修正

--- src/models/user.ts
+++ src/models/user.ts
@@ -1,16 +1,14 @@
 import Book from "./book";
 
 type User = {
-    Id: number;
-	UserName: string;
-	Email: string;
-	Password: string;
-	Avatar: string;
-	Role: number;
-	CreatedAt: Date;
-	UpdatedAt: Date;
-	DeletedAt: Date;
-	UserBooks: Book[];
+	userName: string;
+	email: string;
+	// Avatar: string;
+	// Role: number;
+	// CreatedAt: Date;
+	// UpdatedAt: Date;
+	// DeletedAt: Date;
+	// UserBooks: Book[];
 }
 
 export default User;
